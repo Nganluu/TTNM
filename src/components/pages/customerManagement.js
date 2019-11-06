de57c31ffbaa7d6f9e25a3f40bc47baadb900087
nganluu <nganltt712@gmail.com>
2019-11-06 15:03:32 +0700

    fix css

--- src/components/pages/customerManagement.js
+++ src/components/pages/customerManagement.js
@@ -8,12 +8,15 @@ export default class customerManagement extends Component {
                 <div>
                     <Navbar />
                 </div>
-                <div className="row">
-                    <div className="col-2" style={{boxShadow: "2px 1px #888888", position: "fixed", height: "100%"}}>
+                <div className="row" >
+                    <div className="col-2" style={{ boxShadow: "2px 1px #e8e8e8", height: "100%", position: "fixed"}}>
                         <Sidebar />
                     </div>
-                    <div className="col-9">
-
+                    <div className="col-9" style={{marginLeft: "20rem"}} >
+                            abcs
+                            <div>
+                                abbf
+                            </div>
                     </div>
                 </div>
             </div>
